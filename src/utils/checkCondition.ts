3a2dcc10b7c0a97b1c8bf1439b47a0b12fe62147
Abdelmounim AINOU <ainou.abdelmounim@gmail.com>
2024-06-05 10:27:58 +0100

    fix(checkCondition): check if the frensh condtion is exist or not

--- src/utils/checkCondition.ts
+++ src/utils/checkCondition.ts
@@ -1,9 +1,10 @@
 import { TEmailError } from '@/hooks/useEmailValidation';
 import { isFrance } from './isFrance';
+import { trimAll } from './trimAll';
 
 export const checkCondition = (codeSource: string, lang: string, brand: string) => {
   const conditionOK = '*Oka&iuml;di la bonne id&eacute;e, la bonne identit&eacute;.';
-  const conditionOB = "*Oba&iuml;bi, la plus belle id&eacute;e d'amour.";
+  const conditionOB = '*Oba&iuml;bi, la plus belle id&eacute;e d&rsquo;amour.';
   const error: TEmailError = {
     title: 'Condition',
     message: '',
@@ -11,21 +12,21 @@ export const checkCondition = (codeSource: string, lang: string, brand: string) 
 
   if (isFrance(lang)) {
     if (brand === 'ok') {
-      if (!codeSource.includes(conditionOK)) {
+      if (!trimAll(codeSource).includes(trimAll(conditionOK))) {
         error.message += `<p>Condition <b>"${conditionOK}"</b> is missing </p>`;
       }
     } else {
-      if (!codeSource.includes(conditionOB)) {
+      if (!trimAll(codeSource).includes(trimAll(conditionOB))) {
         error.message += `<p>Condition <b>"${conditionOB}"</b> is missing </p>`;
       }
     }
   } else {
     if (brand === 'ok') {
-      if (codeSource.includes(conditionOK)) {
+      if (trimAll(codeSource).includes(trimAll(conditionOK))) {
         error.message += `<p> Please remove <b>"${conditionOK}"</b> </p>`;
       }
     } else {
-      if (codeSource.includes(conditionOB)) {
+      if (trimAll(codeSource).includes(trimAll(conditionOB))) {
         error.message += `<p> Please remove  <b>"${conditionOB}"</b> </p>`;
       }
     }
